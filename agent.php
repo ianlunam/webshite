5524be32ae2a72a761ab86237eb6179f27b75306
Ian Lunam <ian.lunam@canopy-cloud.com>
2018-01-06 15:11:25 +0000

    First Cut of bits

---
+++ agent.php
@@ -0,0 +1,7 @@
+<html><head><title>Your User Agent</title></head>
+<body>
+<h2>Your User Agent</h2>
+<?php print($_SERVER['HTTP_USER_AGENT']); ?>
+<h2>Your IP</h2>
+<?php print($_SERVER['REMOTE_ADDR']); ?>
+</body></html>
---
+++ temp.php
@@ -0,0 +1,46 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" /> 
+    <META HTTP-EQUIV="refresh" CONTENT="60">
+    <title>Temperatures</title>
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+<?php
+print("['Time', 'Temperature'],");
+$mysqli = new mysqli("localhost", "ian", null, "temps");
+if ($result = $mysqli->query("select time(cap_time) as c_time, temperature from history b order by cap_time desc LIMIT 200")) {
+while($row = $result->fetch_assoc()){
+  print("['" . $row['c_time'] . "', " . $row['temperature'] . "],\n");
+}
+    /* free result set */
+    $result->close();
+}
+mysqli_close($mysqli);
+?>
+        ]);
+
+        var options = {
+          title: 'Temperature C',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+<center>
+    <div id="chart_div" style="width: 900px; height: 500px;"></div>
+<a href='temp.php'>Temperatures</a><br/>
+<a href='humid.php'>Humidity</a><br/>
+<a href="/">Click here to go back</a>
+</center>
+  </body>
+</html>
