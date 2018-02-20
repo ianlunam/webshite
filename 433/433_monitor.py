c24134169927a751d3694d09514595571cd8d0cf
Ian Lunam <ian.lunam@digital.hmrc.gov.uk>
2018-02-20 11:38:51 +0000

    Resiliance

--- 433/433_monitor.py
+++ 433/433_monitor.py
@@ -14,6 +14,7 @@ dbconfig = {
   "user":     "root"
 }
 db = mysql.connector.connect(**dbconfig)
+db.ping(True)
 cursor = db.cursor()
 
 proc = subprocess.Popen(['/usr/local/bin/rtl_433','-F','json','-q'],stdout=subprocess.PIPE)
@@ -28,32 +29,43 @@ while True:
         "temp": data['temperature_C'],
         "battery": data['battery']
       }
-      print "OS Temperature: {0}".format(data['temperature_C'])
-      sys.stdout.flush()
       try:
         cursor.execute(temp_insert, temp_data)
         db.commit()
-      except Exception as err:
+        print "OS Temperature: {0}".format(data['temperature_C'])
+      except mysql.connector.errors.IntegrityError:
         pass
+      except mysql.connector.errors.OperationalError:
+        print "Reconnecting"
+        try:
+          db.reconnect()
+        except Exception:
+          pass
+      except Exception as err:
+        print "Generic temp error: {0}".format(type(err))
     elif data['id'] == 145433412:
       oil_data = {
         "time": data['time'],
         "temp": data['temperature_C'],
         "depth": data['depth']
       }
-      print "Tank: depth: {0} temp: {1}".format(data['depth'], data['temperature_C'])
-      sys.stdout.flush()
       try:
         cursor.execute(oil_insert, oil_data)
         db.commit()
+        print "Tank: depth: {0} temp: {1}".format(data['depth'], data['temperature_C'])
+      except mysql.connector.errors.OperationalError:
+        print "Reconnecting"
+        try:
+          db.reconnect()
+        except Exception:
+          pass
       except Exception as err:
-        pass
+        print "Generic oil error: {0}".format(type(err))
     else:
-      print "".join(['{0}: {1} '.format(k, v) for k,v in data.iteritems()])
-      sys.stdout.flush()
+      print "Dafuq? " + "".join(['{0}: {1} '.format(k, v) for k,v in data.iteritems()])
   else:
     break
+  sys.stdout.flush()
 
 cursor.close()
 db.close()
-
